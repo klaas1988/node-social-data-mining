f38626725f093b8a8e2c70620699519245b8b3b8
Klaas Heidstra <klaas@heidstra.com>
2012-11-24 20:32:31 -0500

    Ignore credentials file and add a sample credentials file

---
+++ SAMPLE.credentials.js
@@ -0,0 +1,8 @@
+'use strict';
+
+exports.twitter = {
+    consumer_key: 'your consumer key here',
+    consumer_secret: 'your consumer secret here',
+    access_token_key: 'your access token key here',
+    access_token_secret: 'your access token secret here'
+};
